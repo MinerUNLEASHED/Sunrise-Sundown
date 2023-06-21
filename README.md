38bb63f4bfb03a8dca5b409885866e3ee87180f0
MinerUNLEASHED <2020tray@gmail.com>
2023-06-20 22:12:43 -0500

    All Added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Sunrise-Sundown
  A Cool Little Program To Tell The Sunrise And Sundown Times In Your Local City
+
+# Python Libraries
+ Requests, Beautiful Soup 4, Tkinter, and PIL (Or More Commonly Known As Pillow)
+
+# How It Works
+ Just put in the local city url from the specific website, and get to know when your sunrise and sundown down is in your city!
---
+++ Sunrise-Sundown.py
@@ -0,0 +1,54 @@
+import requests
+import bs4
+import tkinter as tk
+from tkinter.font import Font
+from PIL import Image, ImageTk
+
+input_city_link = input('Input City From "timeanddate.com/sun/usa/...": ')
+sun_res = requests.get(input_city_link)
+soup = bs4.BeautifulSoup(sun_res.text, 'lxml')
+for item in soup.select('.dn-mob'):
+    sun_times = item.text
+sun_times = sun_times.split('pm')
+sun_times[0] += 'pm'
+
+pre_image_items = []
+for item in soup.select('div main article section div img'):
+    pre_image_items += str(item)
+img_url = "".join(pre_image_items[pre_image_items.index('c', (pre_image_items.index('/')+1)):(pre_image_items.index('g', pre_image_items.index('p')))+1])
+img_url = requests.get('https://'+img_url)
+given_image = open('../given_image.png', 'wb')
+given_image.write(img_url.content)
+given_image.close()
+
+display_window = tk.Tk()
+display_window.title('Weather App')
+display_window.geometry('500x500')
+text_font = Font(display_window, size=30)
+time_text_font = Font(display_window, size=20)
+app_frame = tk.Frame(master=display_window, width=400, height=400, bg='#34A2FE')
+presized_image = Image.open('../given_image.png')
+resized_image = presized_image.resize((400, 400))
+input_image = ImageTk.PhotoImage(resized_image)
+image_label = tk.Label(app_frame, image=input_image, height=400, background='#34A2FE')
+sunrise_sunset_label = tk.Label(app_frame, text=f'{sun_times[0]}', bg='#34A2FE', fg='white', font=text_font)
+time_label = tk.Label(app_frame, text=f'{sun_times[1]}', bg='#34A2FE', fg='white', font=time_text_font)
+image_label.pack()
+sunrise_sunset_label.pack()
+time_label.pack()
+app_frame.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
+display_window.mainloop()
+
+
+'''
+reworked tkinter code
+'''
+# display_window = tk.Tk()
+# display_window.title('Weather App')
+# display_window.geometry('400x400')
+# display_window.configure(bg='#34A2FE')
+#
+# img_path = 'given_image.png'
+# img = ImageTk.PhotoImage(Image.open(img_path))
+#
+# display_window.mainloop()
